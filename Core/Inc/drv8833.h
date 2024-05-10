2f96b401e82a9f05939888039b1e36f66fd6e178
Nik <ux1257opevw2016@yandex.ru>
2024-05-10 12:35:36 +0300

    Added DRV8833 supporting.

---
+++ Core/Inc/drv8833.h
@@ -0,0 +1,26 @@
+#ifndef DRV8833_H
+#define DRV8833_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "tim.h"
+#include <stdint.h>
+
+typedef struct 
+{
+    TIM_HandleTypeDef* tim_handle;
+    uint32_t pwm_ch1;
+    uint32_t pwm_ch2;
+} DRV8833_CH_HandleTypeDef;
+
+void DRV8833_init(DRV8833_CH_HandleTypeDef* hdrv);
+void DRV8833_set_raw(DRV8833_CH_HandleTypeDef* hdrv, int value);
+void DRV8833_set_percent(DRV8833_CH_HandleTypeDef* hdrv, int value);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ Core/Src/drv8833.c
@@ -0,0 +1,36 @@
+#include "drv8833.h"
+
+void DRV8833_init(DRV8833_CH_HandleTypeDef *hdrv)
+{
+    if(hdrv == NULL)
+    {
+        return;
+    }
+
+    HAL_TIM_PWM_Start(hdrv->tim_handle, hdrv->pwm_ch1);
+    HAL_TIM_PWM_Start(hdrv->tim_handle, hdrv->pwm_ch2);
+}
+
+void DRV8833_set_raw(DRV8833_CH_HandleTypeDef *hdrv, int value)
+{
+    if(value >= 0)
+    {
+        __HAL_TIM_SET_COMPARE(hdrv->tim_handle, hdrv->pwm_ch1, (uint32_t)value);
+        __HAL_TIM_SET_COMPARE(hdrv->tim_handle, hdrv->pwm_ch2, 0);
+    }
+    else
+    {
+        __HAL_TIM_SET_COMPARE(hdrv->tim_handle, hdrv->pwm_ch1, 0);
+        __HAL_TIM_SET_COMPARE(hdrv->tim_handle, hdrv->pwm_ch2, (uint32_t)(-value));
+    }
+}
+
+void DRV8833_set_percent(DRV8833_CH_HandleTypeDef *hdrv, int value)
+{
+    value = (value > 100) ? 100 : 
+            (value < -100) ? -100 : 
+            value;
+
+    int ccr_val = (int)(hdrv->tim_handle->Init.Period) * value / 100;
+    DRV8833_set_raw(hdrv, ccr_val);
+}
